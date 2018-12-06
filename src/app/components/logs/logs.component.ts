7b6199426be770efb50917dc16c34f9549e49916
Birdvision <birdvision@Birdvisions-iMac.local>
2018-12-06 22:40:55 +0100

    add missing logs files becasue they were hidden in .gitignore

---
+++ src/app/components/logs/logs.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LogService  } from '../../services/log.service';
+
+import { Log } from '../../models/log';
+
+@Component({
+selector: 'app-logs',
+templateUrl: './logs.component.html',
+styleUrls: ['./logs.component.css']
+})
+export class LogsComponent implements OnInit {
+logs: Log [];
+
+constructor(private logService: LogService) { }
+
+ngOnInit() {
+  this.logs = this.logService.getlogs();
+}
+onSelect(log: Log) {
+console.log(log);
+this.logService.setFormLog(log);
+}
+}
