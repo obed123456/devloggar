1f3fd73d435e9b7796dd4f614f0aec39e5004441
Birdvision <birdvision@Birdvisions-iMac.local>
2018-12-06 22:27:07 +0100

    add BehaviorSubject. Sibbling can share data now

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -3,11 +3,19 @@ import { Log } from '../models/log';
 
 import {  BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+import { Observable } from 'rxjs/Observable'; 
+import { of } from 'rxjs/Observable/of'
+
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
  logs: Log[];
+
+ private logSource =  new BehaviorSubject<Log>({id: null, text:null, date:null}); 
+
+ selectLog = this.logSource.asObservable();
+
   constructor() { 
     this.logs = [
       { id: '1', text : 'Generated components', date: new Date('12/03/2017 12:54:23')},
@@ -21,4 +29,9 @@ export class LogService {
     return this.logs;
     
   }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
 } 
